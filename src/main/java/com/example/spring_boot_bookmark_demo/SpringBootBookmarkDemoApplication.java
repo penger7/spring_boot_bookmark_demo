6ecebb50c18f2760d45bc9f20e1b18ee76470389
wangpeng <wp18739271@163.com>
2017-11-27 20:47:15 +0800

    第一份文件

---
+++ src/main/java/com/example/spring_boot_bookmark_demo/SpringBootBookmarkDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_bookmark_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookmarkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookmarkDemoApplication.class, args);
+	}
+}
